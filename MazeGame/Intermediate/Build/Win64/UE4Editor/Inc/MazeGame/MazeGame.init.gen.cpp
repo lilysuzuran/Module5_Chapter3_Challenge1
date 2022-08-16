205a990a6116f9b6930f13c4526aef04dce9dce3
LKimABK <109537516+lilysuzuran@users.noreply.github.com>
2022-08-15 20:00:07 -0700

    Version 0.1 --- No Challenges Completed Yet, Just the Skeleton

--- MazeGame/Intermediate/Build/Win64/UE4Editor/Inc/MazeGame/MazeGame.init.gen.cpp
+++ MazeGame/Intermediate/Build/Win64/UE4Editor/Inc/MazeGame/MazeGame.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeMazeGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x3F68EEB2,
-				0xCF90CC64,
+				0x7E9E665A,
+				0x5311FA66,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
